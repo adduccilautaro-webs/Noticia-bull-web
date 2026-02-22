3291ec7c1d67445642af2b95506b7083537f48bb
adduccilautaro-webs <adduccilautaro@gmail.com>
2026-02-22 10:51:05 -0300

    Update App.jsx
    Hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 function App() {
-  return <h1>Hola mundo, la app funciona</h1>;
+  return (
+    <div style={{ 
+      padding: '20px', 
+      textAlign: 'center', 
+      fontFamily: 'Arial',
+      color: 'green',
+      fontSize: '24px'
+    }}>
+      ✅ SI VES ESTO, LA APP FUNCIONA
+      <p>El problema es con Parse o Back4app</p>
+    </div>
+  );
 }
 export default App;
